6e68bf597c4e56be9db3661b5b1c7311afe8d829
Wesley <jw95097@hotmail.com>
2021-07-29 16:45:58 +1000

    Add commented code for navbar menu

--- script.js
+++ script.js
@@ -3,3 +3,29 @@ function changeButton(element) {
     element.src = element.bln ? "public/menu.svg" : "public/x-circle.svg";
     element.bln = !element.bln;
 }
+
+// function showDropdown() {
+//     document.getElementById("#navbar").classList.toggle("show");
+// }
+
+// // Close the dropdown if the user clicks outside of it
+// window.onclick = function(event) {
+//     if (!event.target.matches('.menuButton')) {
+//       var dropdowns = document.getElementsByClassName("navbarMenu");
+//       var i;
+//       for (i = 0; i < dropdowns.length; i++) {
+//         var openDropdown = dropdowns[i];
+//         if (openDropdown.classList.contains('show')) {
+//           openDropdown.classList.remove('show');
+//         }
+//       }
+//     }
+//   }
+
+
+// document.getElementById("#menuButton").addEventListener("click", function()
+// {
+//     document.getElementById("")
+// });
+
+// document.querySelector("#menuButton")
